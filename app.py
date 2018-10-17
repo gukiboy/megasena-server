a07978018350224a0edbf1ea3152f1acf747d6bc
Gustavo Kishima <gustavokishima@gmail.com>
2018-10-17 00:18:41 -0300

    Adds a schedule to pick a number
    This implementation requires a Thread to run the scheduled event.
    It might be possible to implement such a behavior in another way,
    though.
    
    It would be a good idea to make the development environment easier
    to use so other people would be able to code as well.

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-#!flask/bin/python
+#!venv/bin/python
 from flask import Flask
 import random
+import sched
+import time
+import threading
 
 app = Flask(__name__)
 
+pickedNumber = None
+scheduler = sched.scheduler(time.time, time.sleep)
+
+def pickNewNumber():
+    global pickedNumber
+    pickedNumber = random.randint(0,10)
+    print(str(pickedNumber) + ' was picked!')
+
 @app.route('/')
 def index():
-    return str(random.randint(0,10))
+    return str(pickedNumber)
+
+scheduler.enter(10,1,pickNewNumber, ())
+scheduler.enter(50,1,pickNewNumber, ())
+
+t = threading.Thread(target=scheduler.run)
+t.start()
+
 
 if __name__ == '__main__':
-    app.run(host='192.168.2.108', debug=True)
+    app.run(host='10.0.0.105', debug=True)
